c5e3611e7962cdf303cfe4ae13056de0661fa370
lucas-noleto <lucas_noleto@outlook.com>
2025-04-16 17:35:26 -0300

    Revisao

---
+++ src/ResumaoDados/Main.java
@@ -0,0 +1,27 @@
+package ResumaoDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        //Array
+        String[] nomeninjaArray = new String[3];
+        nomeninjaArray[2] = "Obtio";
+
+        //Listas
+        //Lista é um pouco mais lenta
+        //Listas sao dinamicas
+        List<String> nomeninjaLista = new ArrayList<>();
+        nomeninjaLista.add("hatake Kakashi");
+
+        //Pilha
+        // existe uma ordem
+        Stack<String> exemploPilha = new Stack<>();
+        exemploPilha.push("ninja
+
+
+
+    }
+}
